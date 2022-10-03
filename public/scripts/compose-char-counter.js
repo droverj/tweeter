f18eecc816855806fb5e34fe47fd0e4b036db41d
Jodie Drover <jodiedrover@live.com>
2022-10-03 10:46:48 -0400

    Update counter based on text input

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -8,7 +8,19 @@ const characterCount = function() {
     textInput.on('input', function () {
       const maxChars = 140;
       const currentCount = this.value.length;
-      console.log(maxChars - currentCount);
+      // return maxChars - currentCount;
+      const result = maxChars - currentCount;
+      const counter = $(this).siblings('.counter');
+      counter[0].value = result;
     })
   })
 };
+
+// $( document.body ).click(function() {
+//   $( "div" ).each(function( i ) {
+//     if ( this.style.color !== "blue" ) {
+//       this.style.color = "blue";
+//     } else {
+//       this.style.color = "";
+//     }
+//   });
