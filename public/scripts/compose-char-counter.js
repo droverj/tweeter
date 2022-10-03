d00f930f78e6aad776209f826fd38e20fae493b5
Jodie Drover <jodiedrover@live.com>
2022-10-02 20:25:49 -0400

    Create function which returns character count

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,1 +1,14 @@
-console.log("this is a test")
+// const { text } = require("body-parser");
+
+const characterCount = function() {
+  $(document).ready(function() {
+
+    const textInput = $('#tweet-text');
+
+    textInput.on('input', function () {
+      const maxChars = 140;
+      const currentCount = this.value.length;
+      console.log(maxChars - currentCount);
+    })
+  })
+};
