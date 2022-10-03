7b3795773306309bb2ea833136bcfd97d4669bc0
Jodie Drover <jodiedrover@live.com>
2022-10-03 10:55:37 -0400

    Turn counter red while invalid

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,26 +1,17 @@
-// const { text } = require("body-parser");
-
 const characterCount = function() {
   $(document).ready(function() {
-
     const textInput = $('#tweet-text');
 
-    textInput.on('input', function () {
+    textInput.on('input', function() {
       const maxChars = 140;
       const currentCount = this.value.length;
-      // return maxChars - currentCount;
       const result = maxChars - currentCount;
       const counter = $(this).siblings('.counter');
-      counter[0].value = result;
-    })
-  })
-};
 
-// $( document.body ).click(function() {
-//   $( "div" ).each(function( i ) {
-//     if ( this.style.color !== "blue" ) {
-//       this.style.color = "blue";
-//     } else {
-//       this.style.color = "";
-//     }
-//   });
+      counter[0].value = result;
+      if (result < 0) {
+        counter[0].style.color = 'red';
+      }
+    });
+  });
+};
