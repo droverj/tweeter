98a331032d547de13b0c4cfed5a6fb3a07aed5b7
Jodie Drover <jodiedrover@live.com>
2022-10-05 13:11:22 -0400

    Remove comments, refactor

--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -1,47 +1,28 @@
-// $(document).ready(() => {
-//   console.log('document ready')
-// })
-
 function getTweets() {
   $.get('/tweets').then((data) => {
-    
     for (const tweets of data) {
-      const $user = addUser(tweets);
-      // const $tweet = addTweet(tweets);
-      // const $date = addDate(tweets);
+      const $user = addTweet(tweets);
       $('.tweets').prepend($user);
     }
-
-  })
+  });
 }
 
+// A function which returns user data as html
 function addTweet(tweets) {
-  const $tweet = $('<article>').text(tweets.content.text);
-  return $tweet;
-}
-
-function addDate(tweets) {
-  const $date = $('<footer>').text(tweets.created_at);
-  const symbols = $('<span>').text("symbols here");
-  $date.append(symbols);
-  return $date;
-}
-
-
-function addUser(tweets) {
-
+  // Create a div for each tweet
   const $newDiv = $('<div>').addClass("new-tweet");
   $('.tweets').append($newDiv);
 
+  // Create a div for user and icon for styling
   const $user = $('<div>').addClass("user");
   const $username = $('<h3>').text(tweets.user.name);
-  const $handle = $('<h3 class="handle">').text(tweets.user.handle);
-
   const $icon = $('<img id="icon">');
   $icon.attr('src', tweets.user.avatars);
   $icon.appendTo('#icon');
 
-  const $header = $('<header id="tweet">');
+  const $handle = $('<h3>').text(tweets.user.handle);
+  const $header = $('<header>');
+
   $user.append($icon, $username);
   $header.append($user);
   $header.append($handle);
@@ -58,28 +39,14 @@ function addUser(tweets) {
   return $newDiv;
 }
 
-//   <div class="user">
-//     <i id="tweetButton" class="fa-solid fa-angles-down"></i>
-//     <h3>username here</h3>
-//   </div>
-//     <h3 class="handle">handle here</h3>
-// // </header>
-
-// <article> TWEET HERE</article>
-// <footer> FOOTER HERE
-// <span>symbols here</span>
-// </footer>
-
-
-
 $(() => {
-  console.log('document ready')
+  console.log('document ready');
   getTweets();
 
-  const $newTweet = $("")
+  const $newTweet = $("");
 
   $newTweet.on("event", event => {
     event.preventDefault();
-  })
-})
+  });
+});
 
